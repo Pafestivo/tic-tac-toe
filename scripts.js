64328c440310bf48fe4fef8e7448b9f49eb72077
Or <orrodrigez1@gmail.com>
2022-11-27 22:09:09 +0200

    Make marker placement and turn swapping functions

--- scripts.js
+++ scripts.js
@@ -1,18 +1,32 @@
 //game board module
 const gameBoard = (() =>{
+  let circleTurn
   const allCells = document.querySelectorAll('[data-cell]');
   allCells.forEach(cell => {
     cell.addEventListener('click', clickHandler, {once: true});
   })
 
-  function clickHandler() {
-    console.log('clicked');
+  function clickHandler(e) {
+    const cell = e.target;
+    const currentMarker = circleTurn ? "o" : "x";
+    placeMarker(cell, currentMarker);
+    //check for winner
+    //check for draw
+    swapTurns();
+  }
+
+  function placeMarker(cell, currentMarker) {
+    cell.classList.add(currentMarker);
+  }
+
+  function swapTurns() {
+    circleTurn = !circleTurn;
   }
 })()
 
 //players factory:
-function setPlayer(name, sign) {
-  return {name, sign};
+function setPlayer(name, marker) {
+  return {name, marker};
 }
 const playerOne = setPlayer('player1', 'X');
 const playerTwo = setPlayer('player2', 'O');
